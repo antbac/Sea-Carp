1422125d83a4512a966e982528dc53179cf39417
Anton Backstrom <antonbcm@gmail.com>
2023-06-05 11:08:39 +0200

    Did some stuff

---
+++ SeaCarp.CrossCutting/Extensions/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace SeaCarp.CrossCutting.Extensions;
+
+public static class EnumExtensions
+{
+    public static string GetDescription(this Enum value)
+    {
+        var fieldInfo = value.GetType().GetField(value.ToString());
+
+        return fieldInfo?.GetCustomAttributes(typeof(DescriptionAttribute), false) is DescriptionAttribute[] attributes && attributes.Any()
+            ? attributes.First().Description
+            : value.ToString();
+    }
+}
---
+++ SeaCarp.Presentation/ViewModels/Challenges/Challenge.cs
@@ -0,0 +1,10 @@
+﻿namespace SeaCarp.Presentation.ViewModels.Challenges;
+
+public class Challenge
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Points { get; set; }
+    public string[] Hints { get; set; }
+    public ChallengeType Type { get; set; }
+}
