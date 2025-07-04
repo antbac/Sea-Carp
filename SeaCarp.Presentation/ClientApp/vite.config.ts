63111f885676434d957a39eedce4143bd2a5d90a
Peter <jsEklund@users.noreply.github.com>
2025-07-04 15:26:35 +0200

    Fix Vite proxy

--- SeaCarp.Presentation/ClientApp/vite.config.ts
+++ SeaCarp.Presentation/ClientApp/vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    port: 5173,
-    cors: {
-      origin: "http://localhost:5173"
-    }
-  }
-})
+  server: {
+    // port: 5000,
+    // cors: {
+    //   origin: "http://localhost:5173",
+    // },
+    proxy: {
+      "^/api|images*": {
+        target: "https://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
